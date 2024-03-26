ec2d810afecfa11ecb4da5cf06befbb632418d9a
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2024-03-26 16:41:03 -0300

    pathing, path lib

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,17 @@ Teoria das rotas
 */
 // importação do express e dos tipos requset e response
 import express, { Request, Response } from 'express'
+import path from 'path'
 import mainRoutes from './routes/index'
 import painelRoutes from './routes/painel'
 
 // função do express na constante server
 const server = express()
+
+// define uma pasta estática - torna publico
+// deve observar sempre o caminho de execução do servidor
+// a lib 'path' auxilia nessa questão, pegando o caminho absoluto do diretório
+server.use(express.static(path.join(__dirname, '../public')))
 
 // inserção das rotas no servidor
 server.use('/', mainRoutes)
